d9bf3789b848a6a3ad45aaf8e8efd5347c09d0d1
Alvin <162141395+alvo-254@users.noreply.github.com>
2025-08-30 17:48:04 +0300

    Fix formatting in settings.py for INSTALLED_APPS

--- crm/settings.py
+++ crm/settings.py
@@ -1,5 +1,7 @@
-INSTALLED_APPS += ['django_crontab']
-"django_crontab",
+INSTALLED_APPS = [
+    ...
+    "django_crontab",
+]
 CRONJOBS = [
-    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+    ("*/5 * * * *", "crm.cron.log_crm_heartbeat"),  # runs every 5 minutes
 ]
